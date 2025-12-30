9b5605c0dd51804e96f3c8723995a7a44387c256
lou-kail <lou.kail@edu.esiee.fr>
2025-12-30 18:39:17 +0100

    feat: initiated geo and histogram

--- main.py
+++ main.py
@@ -4,22 +4,39 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash()
+app = Dash(__name__)
 
-# Requires Dash 2.17.0 or later
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+app.layout = html.Div([
+    html.H1("Dashboard", style={'textAlign': 'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
+
+    html.Div([
+        dcc.Graph(id='graph-histogram', style={'width': '48%', 'display': 'inline-block'}),
+        dcc.Graph(id='graph-map', style={'width': '48%', 'display': 'inline-block'})
+    ])
+])
+
 
 @callback(
-    Output('graph-content', 'figure'),
+    Output('graph-histogram', 'figure'),
+    Output('graph-map', 'figure'),
     Input('dropdown-selection', 'value')
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graphs(country_value):
+    dff = df[df.country == country_value]
+
+    fig_hist = px.bar(dff, x='year', y='pop', title=f"Population : {country_value}")
+
+    fig_map = px.choropleth(
+        dff,
+        locations="country",
+        locationmode="country names",
+        color="lifeExp",
+        title="Localisation"
+    )
+
+    return fig_hist, fig_map
+
 
 if __name__ == '__main__':
     app.run(debug=True)
