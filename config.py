e15c56679c3a22a88d995836f2502182a257bf4e
lou-kail <lou.kail@edu.esiee.fr>
2026-01-01 21:53:59 +0100

    feat(config): added species to fetch in config

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import os
 
+TAXON_IDS = {
+    "HUMPBACK_WHALE": 137092,
+    "BLUE_WHALE": 137090,
+    "ORCA": 137102,
+    "DOLPHIN": 137094,
+}
+
 os.makedirs("data/csv", exist_ok=True)
 
 df = pd.read_csv('data/txt/occurrence.txt', sep='\t')
