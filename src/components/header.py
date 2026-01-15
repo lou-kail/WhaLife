713c55ed21857ec1acda6da9668c73f6123949a3
Théo Séré <104022394+theo-sere@users.noreply.github.com>
2026-01-15 11:48:00 +0100

    Add custom font and update header styling
    Introduces a custom CSS file to define the 'Gluten' font and applies it to the header component. The header's style is updated for improved appearance, including font family, size, and layout adjustments.

--- src/components/header.py
+++ src/components/header.py
@@ -4,7 +4,7 @@ def header():
     #return html.H1("Dashboard", style={'textAlign': 'center'})
     return html.Div([
         html.Img(src='/assets/LogoWhalife.png', alt='image', style={'height': '200px', 'width': '200px'}),
-        html.H1("WhaLife", style={'font-family':'/assets/Gluten/Gluten-Light.ttf', 'textAlign': 'center'})
+        html.H1("WhaLife", style={'fontFamily':'Gluten', 'fontSize':'4rem', 'textAlign': 'center', 'marginLeft' : '15px', 'marginTop' : '80px'})
         ],
         style={
             'display': 'flex',
