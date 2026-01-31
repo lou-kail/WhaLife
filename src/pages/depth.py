a440ffa411eae8430629aa3290ba4189e13eed58
Théo Séré <104022394+theo-sere@users.noreply.github.com>
2026-01-31 21:53:00 +0100

    Correction for the slider of the depth page depth.py

--- src/pages/depth.py
+++ src/pages/depth.py
@@ -1,32 +1,39 @@
 from dash import html, dcc, Output, Input, State, callback
 import plotly.express as px
 import pandas as pd
+import math  # Nécessaire pour l'arrondi
 
 
 def layout_depth(df):
-    max_depth = int(df['bathymetry'].max())
-    step_marks_depth = max_depth // 5
-    return html.Div([
-    html.H2("Analysis by Depth"),
-    html.Label("Select Depth:"),
+    real_max = df['bathymetry'].max()
+    real_min = df['bathymetry'].min()
+    step = 50
+    slider_max = math.ceil(real_max / step) * step
+    slider_min = math.floor(real_min / step) * step
+    step_marks_depth = int(slider_max // 5)
+    if step_marks_depth == 0: step_marks_depth = 1
 
-    html.Div([
-        html.Label("Filter by depth (meters) :"),
-        dcc.RangeSlider(
-            min=df['bathymetry'].min(),
-            max=df['bathymetry'].max(),
-            step=50,
-            value=[df['bathymetry'].min(), df['bathymetry'].max()],
-            marks={i: f'{i}m' for i in range(0, max_depth + 1, step_marks_depth)},
-            tooltip={"placement": "bottom", "always_visible": True},
-            id='depth-slider'
-        )
-    ], style={'padding': '20px'}),
-    html.Div([
-        dcc.Graph(id='graph-depth-map', style={'width': '48%', 'display': 'inline-block'}),
-        dcc.Graph(id='graph-depth-hist', style={'width': '48%', 'display': 'inline-block', 'float': 'right'})
+    return html.Div([
+        html.H2("Analysis by Depth"),
+        html.Label("Select Depth:"),
+
+        html.Div([
+            html.Label("Filter by depth (meters) :"),
+            dcc.RangeSlider(
+                min=slider_min,
+                max=slider_max,
+                step=step,
+                value=[slider_min, slider_max],
+                marks={i: f'{i}m' for i in range(int(slider_min), int(slider_max) + 1, step_marks_depth)},
+                tooltip={"placement": "bottom", "always_visible": True},
+                id='depth-slider'
+            )
+        ], style={'padding': '20px'}),
+        html.Div([
+            dcc.Graph(id='graph-depth-map', style={'width': '48%', 'display': 'inline-block'}),
+            dcc.Graph(id='graph-depth-hist', style={'width': '48%', 'display': 'inline-block', 'float': 'right'})
+        ])
     ])
-])
 
 
 @callback(
@@ -38,6 +45,7 @@ def layout_depth(df):
 def update_depth_page(val_range, stored_data):
     if not stored_data: return px.scatter(), px.histogram()
     dff = pd.DataFrame(stored_data)
+
     dff = dff[(dff['bathymetry'] >= val_range[0]) & (dff['bathymetry'] <= val_range[1])]
 
     fig_map = px.scatter_map(
@@ -64,8 +72,8 @@ def update_depth_page(val_range, stored_data):
     fig_hist.update_yaxes(matches=None, showticklabels=True)
     fig_hist.update_xaxes(matches='x')
     fig_hist.update_layout(
-        plot_bgcolor='rgba(0,0,0,0)',  # Fond du graphique transparent
-        paper_bgcolor='rgba(0,0,0,0)',  # Fond du papier transparent
-        font_color='black'  # (Optionnel) assure que le texte reste lisible
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        font_color='black'
     )
     return fig_map, fig_hist
